18c1db4f623c30f1e5f414c279d559a2fc2bf222
David Ladipo <64803207+deedevs@users.noreply.github.com>
2020-09-28 13:44:57 +0300

    basics-01

--- basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,41 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+function createAndWrite(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
 }
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWrite("+", initialResult, enteredNumber);
+}
 
-let calculateDescription = `(${defaultResult} + 10)*3/2-1`;
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWrite("-", initialResult, enteredNumber);
+}
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWrite("*", initialResult, enteredNumber);
+}
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWrite("/", initialResult, enteredNumber);
+}
 
-outputResult(currentResult, calculateDescription);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
